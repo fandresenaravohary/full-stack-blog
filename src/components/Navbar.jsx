e30d0c031b26bb6038ba2a21bd798e94c6b27a42
Fandresena Ravohary <hei.fandresena@gmail.com>
2024-12-14 11:06:12 +0300

    fix: replace <a> tags with <Link> components for internal navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,28 +22,28 @@ const Navbar = () => {
           className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out
                 ${open ? "-right-0" : "-right-[100%]"}`}
         >
-          <a href="">Home</a>
-          <a href="">Trending</a>
-          <a href="">Most Popular</a>
-          <a href="">About</a>
-          <a href="">
+          <Link href="">Home</Link>
+          <Link href="">Trending</Link>
+          <Link href="">Most PopulLinkr</Link>
+          <Link href="">About</Link>
+          <Link href="">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login👋
             </button>
-          </a>
+          </Link>
         </div>
       </div>
       {/*DESKTOP MENU */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="">Home</a>
-        <a href="">Trending</a>
-        <a href="">Most Popular</a>
-        <a href="">About</a>
-        <a href="">
+        <Link href="">Home</Link>
+        <Link href="">Trending</Link>
+        <Link href="">Most PopulLinkr</Link>
+        <Link href="">About</Link>
+        <Link href="">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
             Login👋
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
